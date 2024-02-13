05df363a2d5bc8690c0a53703094ffacfccce839
Chris Johnson <johnson.chris00@gmail.com>
2024-02-13 13:33:11 -0600

    adds express server

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,11 +1,23 @@
 import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
+import dotenv from "dotenv"
+dotenv.config()
+
+const { PORT = 3001 } = process.env
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true,
+    proxy: {
+      "/api": {
+        target: `http://localhost:${PORT}`,
+        changeOrigin: true,
+      },
+    },
+  },
+  build: {
+    outDir: "dist/app",
   },
   test: {
     globals: true,
