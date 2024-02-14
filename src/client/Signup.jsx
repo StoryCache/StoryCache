a00005f63b8623dac350479fc79349b6afd4e974
apalazzomusic <apalazzomusic@gmail.com>
2024-02-13 19:37:27 -0800

    initializes useState hooks

---
+++ src/client/Signup.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+// import './Signup.css';
+
+const Signup = () => {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+}
