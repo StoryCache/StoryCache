3c2433935bfff8e987c3bae310879f32b709f64e
apalazzomusic <apalazzomusic@gmail.com>
2024-02-13 19:17:23 -0800

    fixes migration issues, disables state mgmt

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,8 +1,8 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
 // import { Provider } from "react-redux"
-import App from "./App";
-import Login from "./Login"
+import App from "./App.jsx";
+import Login from "./Login.jsx"
 // import { store } from "./app/store"
 import "./index.css"
 // import * as ReactDOM from"react-dom/client";
