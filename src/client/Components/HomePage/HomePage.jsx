3836019722445b611054962335676b871dd9b002
mayson124 <mayson0717@gmail.com>
2024-02-13 16:33:25 -0800

    Add CSS and Change Files Added CSS to cards and change file layout
    Co-authored-by: Alex Palazzo <apalazzomusic@gmail.com>
    Co-authored-by: Jenny Ouk <jenny.ouk@gmail.com>
    Co-authored-by: Chris Johnson <johnson.chris00@gmail.com>
    Co-authored-by: Margaret Hatch <margaret.l.hatch@gmail.com>

---
+++ src/client/Components/HomePage/HomePage.jsx
@@ -0,0 +1,56 @@
+import "./HomePage.css"
+import { useState, useEffect } from "react";
+
+
+const HomePage = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [catalog, setCatalog] = useState([]);
+
+  const handleSearch = async () => {
+    try {
+      const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchQuery}&key=AIzaSyALO2yhbqZ3qDdUmkAtsmMFsGF0V4JMkEA`);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      setSearchResults(data.items); // Assuming items contain search results
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const addToCatalog = (book) => {
+    setCatalog([...catalog, book]);
+  };
+
+  return (
+    <div className="App">
+      <header>
+        <h1>My Book Catalog</h1>
+      </header>
+      <div className="search-bar">
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          placeholder="Search for books..."
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
+      <div className="card-container">
+        {searchResults.map((book) => (
+          // let picture = book.volumeInfo.imageLinks.medium
+          <div className="card" key={book.id}>
+            {/* Display book information */}
+            <h2>{book.volumeInfo.title}</h2>
+            <p>{book.volumeInfo.authors.join(', ')}</p>
+            <img src={book.volumeInfo.imageLinks.smallThumbnail} alt="bookimg"></img>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default HomePage
--- src/client/app/store.ts
+++ src/client/app/store.ts
@@ -1,8 +1,8 @@
 import type { Action, ThunkAction } from "@reduxjs/toolkit"
 import { combineSlices, configureStore } from "@reduxjs/toolkit"
 import { setupListeners } from "@reduxjs/toolkit/query"
-import { counterSlice } from "../features/counter/counterSlice"
-import { quotesApiSlice } from "../features/quotes/quotesApiSlice"
+import { counterSlice } from "../Components/counter/counterSlice"
+import { quotesApiSlice } from "../Components/quotes/quotesApiSlice"
 
 // `combineSlices` automatically combines the reducers using
 // their `reducerPath`s, therefore we no longer need to call `combineReducers`.
