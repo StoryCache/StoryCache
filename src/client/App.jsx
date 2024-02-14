38738242a963e62dc4ba2d01c1a0b20c5a876baa
apalazzomusic <apalazzomusic@gmail.com>
2024-02-13 16:59:41 -0800

    routes to App with react router

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css"
+import "./HomePage.css"
 import { useState, useEffect } from "react";
 
 
--- src/client/main.tsx
+++ src/client/main.tsx
@@ -2,19 +2,31 @@ import React from "react"
 import { createRoot } from "react-dom/client"
 import { Provider } from "react-redux"
 import App from "./App"
-import { store } from "./app/store"
+// import { store } from "./app/store"
 import "./index.css"
+import * as ReactDOM from"react-dom/client";
+import{
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 const container = document.getElementById("root")
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+]);
 
 if (container) {
   const root = createRoot(container)
 
   root.render(
     <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      {/* <Provider store={store}> */}
+        <RouterProvider router={router} />
+        {/* <App /> */}
+      {/* </Provider> */}
     </React.StrictMode>,
   )
 } else {
